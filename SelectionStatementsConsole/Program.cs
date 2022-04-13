1f6f054763e2816a7de654b5cbc78428e879df96
kaylafinkelstein <k.finkelstein4000@gmail.com>
2022-04-13 13:41:27 -0400

    Add project files.

---
+++ SelectionStatementsConsole/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SelectionStatementsConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int favNumber = 10;
+            Console.WriteLine("Try to guess my favorite number");
+            int userGuess = int.Parse(Console.ReadLine());
+
+            if (userGuess < favNumber)
+            {
+                Console.WriteLine("Too Low !");
+            }
+            else if  (userGuess > favNumber)
+            {
+                Console.WriteLine("Too High!");
+            }
+            else 
+            {
+                Console.WriteLine("Nevermind");
+            }
+        }
+    }
+}
